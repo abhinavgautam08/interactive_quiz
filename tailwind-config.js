94ac424fc9b0b01e2df20bd5d8927dad5b0eccaf
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-03-24 20:57:47 +0530

    tailwind

---
+++ tailwind-config.js
@@ -0,0 +1,16 @@
+/* Tailwind config -  abhinavgautam08*/
+tailwind.config = {
+      darkMode: "class",
+      theme: {
+        extend: {
+          colors: { "tertiary-fixed": "#e9ddff", "surface-container": "#e6eeff", "background": "#f8f9ff", "on-tertiary-fixed-variant": "#5516be", "on-tertiary-fixed": "#23005c", "primary": "#4648d4", "surface-bright": "#f8f9ff", "error": "#ba1a1a", "on-surface-variant": "#464554", "outline-variant": "#c7c4d7", "on-error": "#ffffff", "secondary-fixed": "#6ffbbe", "secondary-fixed-dim": "#4edea3", "on-tertiary": "#ffffff", "on-primary": "#ffffff", "on-secondary-fixed": "#002113", "on-secondary": "#ffffff", "surface-container-low": "#eff4ff", "primary-container": "#6063ee", "on-primary-fixed": "#07006c", "on-tertiary-container": "#fffbff", "surface": "#f8f9ff","inverse-on-surface": "#eaf1ff","surface-container-highest": "#d9e3f6","on-primary-container": "#fffbff","surface-variant": "#d9e3f6","surface-dim": "#d0dbed","on-secondary-fixed-variant": "#005236","surface-container-high": "#dee9fc","primary-fixed-dim": "#c0c1ff","surface-container-lowest": "#ffffff","on-primary-fixed-variant": "#2f2ebe","inverse-surface": "#27313f","tertiary-fixed-dim": "#d0bcff","on-surface": "#121c2a","on-background": "#121c2a","on-error-container": "#93000a","on-secondary-container": "#00714d","secondary-container": "#6cf8bb","surface-tint": "#494bd6","tertiary": "#6b38d4","error-container": "#ffdad6","secondary": "#006c49","tertiary-container": "#8455ef","inverse-primary": "#c0c1ff","outline": "#767586","primary-fixed": "#e1e0ff"
+          },
+          fontFamily: {
+            "headline": ["Plus Jakarta Sans"],
+            "body": ["Manrope"],
+            "label": ["Manrope"]
+          },
+          borderRadius: { "DEFAULT": "0.25rem", "lg": "0.5rem", "xl": "0.75rem", "full": "9999px" },
+        },
+      },
+    }
